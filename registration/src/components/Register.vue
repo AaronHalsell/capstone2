6236d561a897da5a542ecd8cc4d8b437a56a8c22
Aaron Halsell <aaron.halsell@gmail.com>
2023-03-07 17:35:03 -0700

    added styles to Register.vue

--- registration/src/components/Register.vue
+++ registration/src/components/Register.vue
@@ -17,6 +17,7 @@
                 telephone: '',
                 address: '',
                 address2: '',
+                isActive: false
             }
         },
         methods: {
@@ -42,71 +43,67 @@
 </script>
 
 <template>
-<h3>Sign Up</h3>
-<form @submit.prevent="handleSubmit">
-    <div>
-        <label>First Name</label>
-        <input type="text" v-model="firstName" placeholder="First Name"/>
+<div class="grid md:grid-cols-2 h-screen ">
+    <!-- <div 
+        class="graphic md:block duration-1000 transition"
+        v-bind:class="{ 'translate-x-full': !isActive, 'hidden': isActive }" 
+    ></div> -->
+    <div 
+        class="mt-7 p-4 md:space-y-36 space-y-20 flex flex-col content-center flex-wrap duration-1000 transition" 
+    >  
+    <img alt="school logo" class="logo" src="../../public/img/logo.png" width="300"/>
+        <div class="space-y-7 lg:w-3/5 w-5/6">
+            <h3 class="text-4xl">Create Account</h3>
+            <form @submit.prevent="handleSubmit" class="space-y-5">
+                <div class='flex justify-between'>
+                    <input type="text" v-model="firstName" placeholder="First Name" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required />
+                    <input type="text" v-model="lastName" placeholder="Last Name" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required />
+                </div>
+                <div>
+                    <input type="email" v-model="email" placeholder="Email" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required />
+                </div>
+                <div>
+                    <input type="password" v-model="password" placeholder="Password" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required/>
+                </div>
+                <div>
+                    <input type="password" v-model="password_confirm" placeholder="Confirm Password" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required />
+                </div>
+                <span @click="isActive = !isActive" class="w-full btn bg-primary hover:bg-secondary text-[#fff] rounded-md p-2 md:text-lg">Sign Up</span>
+                <div class='space-y-5'>
+                    <div>
+                        <input type="text" v-model="telephone" placeholder="Telephone" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required/>
+                    </div>
+                    <div>
+                        <input type="text" v-model="address" placeholder="Address" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required />
+                    </div>
+                    <div>
+                        <input type="text" v-model="address2" placeholder="Address 2 (Optional)" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" />
+                    </div>
+                    <div class='flex justify-between'>
+                        <input type="text" v-model="city" placeholder="City" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required />
+                        <input type="select" v-model="district" placeholder="District (State)" class="border-solid border-2 rounded-md p-2 border-gray md:text-lg" required />
+                    </div>
+                    <div class='flex justify-between'>
+                        <input type="text" v-model="country" placeholder="Country" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required/>
+                        <input type="text" v-model="postalCode" placeholder="Postal Code (Zip)" class="w-full border-solid border-2 rounded-md p-2 border-gray md:text-lg" required />
+                    </div>
+                    <button class="w-full btn bg-primary hover:bg-secondary text-[#fff] rounded-md p-2 md:text-lg">Sign Up</button>
+                </div>
+            </form>
+        <p class="md:text-lg">Already have an Account? <RouterLink to="/login" class="underline font-bold">Log in</RouterLink></p>
     </div>
-
-    <div>
-        <label>Last Name</label>
-        <input type="text" v-model="lastName" placeholder="Last Name"/>
     </div>
-
-    <div>
-        <label>Email</label>
-        <input type="email" v-model="email" placeholder="Email"/>
-    </div>
-
-    <div>
-        <label>Address</label>
-        <input type="text" v-model="address" placeholder="Address"/>
-    </div>
-
-    <div>
-        <label>Address</label>
-        <input type="text" v-model="address2" placeholder="Email"/>
-    </div>
-
-    <div>
-        <label>District</label>
-        <input type="text" v-model="district" placeholder="District"/>
-    </div>
-    
-    <div>
-        <label>City</label>
-        <input type="text" v-model="city" placeholder="City"/>
-    </div>
-
-    
-    <div>
-        <label>Postal Code</label>
-        <input type="text" v-model="postalCode" placeholder="Postal Code"/>
-    </div>
-
-    
-    <div>
-        <label>Country</label>
-        <input type="text" v-model="country" placeholder="Country"/>
-    </div>
-
-    
-    <div>
-        <label>Telephone</label>
-        <input type="text" v-model="telephone" placeholder="Telephone"/>
-    </div>
-
-    <div>
-        <label>Password</label>
-        <input type="password" v-model="password" placeholder="Password"/>
-    </div>
-
-    <div>
-        <label>Confirm Password</label>
-        <input type="password" v-model="password_confirm" placeholder="Confirm Password"/>
-    </div>
-
-    <button>Sign Up</button>
-</form>
+    <div 
+        class="graphic md:block duration-1000 transition"
+        v-bind:class="{ 'order-first': isActive }" 
+    ></div>
+</div>
 </template>
+
+<style>
+    .graphic {
+        background-image: url('../../public/img/graphic.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
